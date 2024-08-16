bae3361e5a50b5eb69d08bbc3dbd40219a776c42
Clare <techclare6@gmail.com>
2024-08-16 16:10:22 +0100

    apply & call

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 // const bookings = []
 // const creatBookings = function(flightNum, numPassenger=1, price=99*numPassenger){
@@ -9,7 +9,7 @@
 //     }
 //     bookings.push(booking)
 //     console.log(bookings);
-    
+
 // }
 
 // creatBookings("FG3445")
@@ -45,26 +45,97 @@
 // return functions from functions
 // call methods on function
 
-
-const oneWord = function(str){
-    return str.replace(/ /g, '').toLowerCase()
-    // return str.toLowerCase().replaceAll(" ", "")
-}
-const upperFirstWord = function(str){
-    const [first, ...others] = str.split(" ")    
-    return [first.toUpperCase(), ...others].join(" ")
-}
+// const oneWord = function(str){
+//     return str.replace(/ /g, '').toLowerCase()
+//     // return str.toLowerCase().replaceAll(" ", "")
+// }
+// const upperFirstWord = function(str){
+//     const [first, ...others] = str.split(" ")
+//     return [first.toUpperCase(), ...others].join(" ")
+// }
 
 // higher order function
-const transformer = function(str, callback){
-    console.log(`Original string: ${str}`);
-    console.log(`Transformed string: ${callback(str)}`);
+// const transformer = function(str, callback){
+//     console.log(`Original string: ${str}`);
+//     console.log(`Transformed string: ${callback(str)}`);
 
-    console.log(`Transformed by: ${callback.name}`);
-    
-}
+//     console.log(`Transformed by: ${callback.name}`);
 
-transformer("Javascript is the best!", upperFirstWord)
-transformer("Javascript is the best", oneWord)
+// }
 
+// transformer("Javascript is the best!", upperFirstWord)
+// transformer("Javascript is the best", oneWord)
+
+// functions returning functions
+// const greet = function(greeting){
+//     return function(name){
+//         console.log(`Hello ${name}, good ${greeting}`);
+
+//     }
+// }
+
+// const greet =(greeting)=>{
+//     return (name)=>{
+//         console.log(`Hello ${name}, good ${greeting}`);
+
+//     }
+// }
+// const greetHerHey = greet("morning")
+// greetHerHey("Clare")
+// greet("evening")("Steven")
+
+// const arg = []
+// const createHelloWorld = function() {
+//     return (...args) => console.log(`Hello World ${args}`);
+
+// };
+// createHelloWorld()(arg)
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iatacode: "LH",
+  bookings: [],
+  book: function (flightNum, name) {
+    this.bookings.push({ flight: `${this.iatacode}${flightNum}`, name });
+
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iatacode} ${flightNum}`
+    );
+  },
+};
+
+console.log(lufthansa.book("234", "Clare"));
+console.log(lufthansa.book("678", "John Smith"));
+console.log(lufthansa);
+
+const eurowings = {
+  airline: "Eurowings",
+  iatacode: "EW",
+  bookings: [],
+};
+const swissArline = {
+  airline: "Swiss Arlines",
+  iatacode: "LX",
+  bookings: [],
+};
+
+const bookNew = lufthansa.book;
+// does not work
+// bookNew(23,"Sarah Williams")
+// the call method
+bookNew.call(eurowings, 23, "sarah williams");
+console.log(eurowings);
+
+bookNew.call(lufthansa, 326, "Mary Cooper");
+console.log(lufthansa);
+
+bookNew.call(swissArline, 940, "Debby Ray");
+console.log(swissArline);
+
+// apply method
+const flightData = [583, "George Cooper"]
+// bookNew.apply(swissArline, flightData)
+bookNew.call(swissArline, ...flightData)
+
+console.log(swissArline);
 
